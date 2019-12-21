d0ef414c4d5490466a52ad8d5901a233de338e1c
srivaishnavi-kotagiri <59123032+srivaishnavi-kotagiri@users.noreply.github.com>
2019-12-21 15:36:58 -0500

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CountryInfo';
+  profileForm = new FormGroup(
+    {
+      
+      CountryCode: new FormControl('', [Validators.required]),
+      
+
+    })
+}
